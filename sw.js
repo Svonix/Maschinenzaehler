9f604b79ab4906b0a270d4c650eeb962fab66e4f
Svonix <svensmart@posteo.de>
2025-11-03 20:23:08 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('mz-v1').then(c=>c.addAll([
-    './','./index.html','./v2_2_5.html','./v2_2_6.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'
-  ])));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
